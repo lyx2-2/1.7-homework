e311970a3f85d47bfa4ed67ffda2dd4e4f8b8b8a
lyx2-2 <Yixuan.Lu2222@gmail.com>
2024-10-02 01:37:47 +0800

    final

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import './index.css';
+
+for(let i=1; i<=9; i++){
+  let output = []
+  for(let j=1; j<=i; j++){
+    // console.log(`${j}×${i}=${i*j}`);
+    output.push(`${j}×${i}=${i*j}`)
+  }
+  console.log(output.join(' '));
+}
+
+
+
